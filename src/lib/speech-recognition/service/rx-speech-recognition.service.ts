9bbd6cb460b58e91398db417700ade8e3159e123
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-09-13 12:58:30 +0200

    Update CLI and angular to version 6

--- src/lib/speech-recognition/service/rx-speech-recognition.service.ts
+++ src/lib/speech-recognition/service/rx-speech-recognition.service.ts
@@ -1,11 +1,7 @@
-import { Observable } from 'rxjs/Observable';
-import { UnaryFunction } from 'rxjs/interfaces';
-import { Subject } from 'rxjs/Subject';
+import { Observable, UnaryFunction, Subject, pipe } from 'rxjs';
 import { Injectable, ApplicationRef, Inject, Optional } from '@angular/core';
 
-import { filter } from 'rxjs/operators/filter';
-import { map } from 'rxjs/operators/map';
-import { pipe } from 'rxjs/util/pipe';
+import { filter, map } from 'rxjs/operators';
 
 import {
   SpeechRecognitionGrammars,
@@ -24,22 +20,15 @@ import {
   SpeechRecognitionNomatchHandler,
   SpeechRecognitionErrorHandler,
   SpeechRecognitionStartHandler,
-  SpeechRecognitionEndHandler,
+  SpeechRecognitionEndHandler
 } from './speech-recognition.token';
 
-import {
-  SpeechGrammarListType,
-  SpeechRecognitionServiceEvent,
-} from '../adapter';
+import { SpeechGrammarListType, SpeechRecognitionServiceEvent } from '../adapter';
 
-import {
-  SpeechRecognitionCommon,
-} from './speech-recognition.common';
-
+import { SpeechRecognitionCommon } from './speech-recognition.common';
 
 @Injectable()
 export class RxSpeechRecognitionService extends SpeechRecognitionCommon {
-
   static resultList: UnaryFunction<Observable<SpeechRecognitionEvent>, Observable<SpeechRecognitionResultList>> = pipe(
     RxSpeechRecognitionService.on('result'),
     map((e: SpeechRecognitionEvent): SpeechRecognitionResultList => e.results)
@@ -47,36 +36,34 @@ export class RxSpeechRecognitionService extends SpeechRecognitionCommon {
 
   private proxy$: Subject<SpeechRecognitionServiceEvent> = new Subject();
 
-
   get $(): Observable<SpeechRecognitionServiceEvent> {
     return this.proxy$ as Observable<SpeechRecognitionServiceEvent>;
   }
 
-
   static on(type: string) {
-    return filter((e: SpeechRecognitionServiceEvent) => (e.type === type));
+    return filter((e: SpeechRecognitionServiceEvent) => e.type === type);
   }
 
   constructor(
     private ref: ApplicationRef,
-
-    @Optional() @Inject(SpeechRecognitionGrammars)
+    @Optional()
+    @Inject(SpeechRecognitionGrammars)
     grammars: SpeechGrammarListType,
-
-    @Optional() @Inject(SpeechRecognitionLang)
+    @Optional()
+    @Inject(SpeechRecognitionLang)
     lang: string,
-
-    @Optional() @Inject(SpeechRecognitionContinuous)
+    @Optional()
+    @Inject(SpeechRecognitionContinuous)
     continuous: boolean,
-
-    @Optional() @Inject(SpeechRecognitionInterimResults)
+    @Optional()
+    @Inject(SpeechRecognitionInterimResults)
     interimResults: boolean,
-
-    @Optional() @Inject(SpeechRecognitionMaxAlternatives)
+    @Optional()
+    @Inject(SpeechRecognitionMaxAlternatives)
     maxAlternatives: number,
-
-    @Optional() @Inject(SpeechRecognitionServiceUri)
-    serviceURI: string,
+    @Optional()
+    @Inject(SpeechRecognitionServiceUri)
+    serviceURI: string
   ) {
     super(grammars, lang, continuous, interimResults, maxAlternatives, serviceURI);
     this.initInternal();
@@ -84,11 +71,11 @@ export class RxSpeechRecognitionService extends SpeechRecognitionCommon {
 
   private initInternal() {
     // set handlers
-    const handler = (e) => {
+    const handler = e => {
       this.proxy$.next(e);
       this.ref.tick();
     };
-    const errHandler = (e) => {
+    const errHandler = e => {
       this.proxy$.error(e);
       this.ref.tick();
     };
@@ -114,7 +101,6 @@ export class RxSpeechRecognitionService extends SpeechRecognitionCommon {
     this.interimResults = this._interimResults;
     this.maxAlternatives = this._maxAlternatives;
     this.serviceURI = this._serviceURI;
-
   }
 
   // The listen() method aims to recognize the grammar associated with the current SpeechRecognition,
@@ -128,11 +114,10 @@ export class RxSpeechRecognitionService extends SpeechRecognitionCommon {
   // 返り値をsubscriveすると聴取が開始され、endのイベントが発生すると聴取を終了します。
   // 途中でunsbscriveすることで聴取を中断します。
   public listen(): Observable<SpeechRecognitionServiceEvent> {
-
-    const listener = Observable.create((ovserver) => {
+    const listener = Observable.create(ovserver => {
       // create subscriotion
       const subscriotion = this.proxy$.subscribe({
-        next: (e) => {
+        next: e => {
           ovserver.next(e);
 
           if (e.type && e.type === 'end') {
@@ -141,7 +126,7 @@ export class RxSpeechRecognitionService extends SpeechRecognitionCommon {
             subscriotion.unsubscribe();
           }
         },
-        error: (e) => ovserver.error(e),
+        error: e => ovserver.error(e)
       });
 
       // Speech Recognition start
@@ -155,5 +140,4 @@ export class RxSpeechRecognitionService extends SpeechRecognitionCommon {
 
     return listener;
   }
-
 }
